947b8cbfb5c72d506256f89156e893d61bc7cdae
Jakub Białas <jakub.bialas@gmail.com>
2012-03-21 22:29:50 +0100

    Added polynomials in discreteobject class

--- discreteobject.cpp
+++ discreteobject.cpp
@@ -1,9 +1,20 @@
 #include "discreteobject.h"
 
-DiscreteObject::DiscreteObject()
-{
+DiscreteObject::DiscreteObject(){
+    //A = null;
+    //B = null;
 }
 
 double DiscreteObject::Symuluj(double input){
+    return input;
+}
 
+DiscreteObject::DiscreteObject(vector<double> n_A, vector<double> n_B){
+    A = n_A;
+    B = n_B;
+}
+
+void DiscreteObject::setAB(vector<double> n_A, vector<double> n_B){
+    A = n_A;
+    B = n_B;
 }
--- discreteobject.h
+++ discreteobject.h
@@ -3,10 +3,24 @@
 
 #include "objectsiso.h"
 
+#include <vector>
+#include <deque>
+
+using namespace std;
+
 class DiscreteObject : public ObjectSISO
 {
+private:
+    vector<double> A; //wielomian A
+    vector<double> B; //wielomian B
+
+    deque<double> stat; //kolejka stanu (poprzednie wartości)
+
 public:
     DiscreteObject();
+    DiscreteObject(vector<double>, vector<double>);
+
+    void setAB(vector<double>, vector<double>);
 
     double Symuluj(double);
 };
